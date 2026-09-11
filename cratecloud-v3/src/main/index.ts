dcf20df101f9fabb23f5a1a6ea1303684765968d
Cornell <cornellb28@gmail.com>
2026-09-11 11:44:12 -0500

    fix: window size update

--- cratecloud-v3/src/main/index.ts
+++ cratecloud-v3/src/main/index.ts
@@ -1533,8 +1533,8 @@ let mainWindow: BrowserWindow | null = null
 function createWindow(): void {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 900,
-    height: 670,
+    width: 1200,
+    height: 1000,
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
